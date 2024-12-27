08fddbe94df8284e995e2383d4e10e4ca4c7cc57
Supervisor <kirillkrylov>
2024-12-27 03:12:59 +0100

    Link repository with application Demo app

---
+++ packages/AtfDemoApp/Schemas/AtfDemoApp_Detail/AtfDemoApp_Detail.js
@@ -0,0 +1,8 @@
+define('AtfDemoApp_Detail', [], function () {
+	return {
+		entitySchemaName: 'AtfDemoApp',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
